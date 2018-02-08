f97336cffd1008f481f0d31b309bdf4f655e373f
Kaliel <maxime@kaliel.fr>
2018-02-08 18:12:14 +0100

    custom fields

--- admin/language/fr/common/reset.php
+++ admin/language/fr/common/reset.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']  = 'Reset your password';
+$_['heading_title']  = 'Réinitialiser le mot de passe';
 
 // Text
-$_['text_password']  = 'Enter the new password you wish to use.';
+$_['text_password']  = 'Saisir votre nouveau mot de passe';
 $_['text_success']   = 'Success: Your password has been successfully updated.';
 
 // Entry
-$_['entry_password'] = 'Password';
-$_['entry_confirm']  = 'Confirm';
+$_['entry_password'] = 'Mot de passe';
+$_['entry_confirm']  = 'Confirmer';
 
 // Error
-$_['error_password'] = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']  = 'Password and password confirmation do not match!';
+$_['error_password'] = 'Le mot de passe doit contenir entre 4 et 20 cacatères !';
+$_['error_confirm']  = 'Les mots de passe ne concordent pas !';
--- admin/language/fr/customer/custom_field.php
+++ admin/language/fr/customer/custom_field.php
@@ -1,47 +1,47 @@
 <?php
 // Heading
-$_['heading_title']        = 'Custom Fields';
+$_['heading_title']        = 'Champ personnalisé';
 
 // Text
 $_['text_success']         = 'Success: You have modified custom fields!';
-$_['text_list']            = 'Custom Field List';
-$_['text_add']             = 'Add Custom Field';
-$_['text_edit']            = 'Edit Custom Field';
-$_['text_choose']          = 'Choose';
-$_['text_select']          = 'Select';
-$_['text_radio']           = 'Radio';
-$_['text_checkbox']        = 'Checkbox';
-$_['text_input']           = 'Input';
-$_['text_text']            = 'Text';
-$_['text_textarea']        = 'Textarea';
-$_['text_file']            = 'File';
+$_['text_list']            = 'Liste de champs personnalisés';
+$_['text_add']             = 'Ajouter un champ personnalisé';
+$_['text_edit']            = 'Modifier ce champ personnalisé';
+$_['text_choose']          = 'Sélection';
+$_['text_select']          = 'Liste de sélection';
+$_['text_radio']           = 'Boutons radio';
+$_['text_checkbox']        = 'Cases à cocher';
+$_['text_input']           = 'Texte';
+$_['text_text']            = 'Champ texte';
+$_['text_textarea']        = 'Grand champ texte';
+$_['text_file']            = 'Fichier';
 $_['text_date']            = 'Date';
-$_['text_datetime']        = 'Date &amp; Time';
-$_['text_time']            = 'Time';
+$_['text_datetime']        = 'Date &amp; Heure';
+$_['text_time']            = 'Heure';
 $_['text_account']         = 'Account';
-$_['text_address']         = 'Address';
+$_['text_address']         = 'Adresse';
 $_['text_affiliate']       = 'Affiliate';
-$_['text_regex']           = 'Regex';
-$_['text_custom_field']    = 'Custom Field';
-$_['text_value']           = 'Custom Field Values';
+$_['text_regex']           = 'Expression régulière';
+$_['text_custom_field']    = 'Champ personnalisé';
+$_['text_value']           = 'Valeurs des champs personnalisés';
 
 // Column
-$_['column_name']          = 'Custom Field Name';
-$_['column_location']      = 'Location';
+$_['column_name']          = 'Nom du champ personnalisé';
+$_['column_location']      = 'Emplacement';
 $_['column_type']          = 'Type';
-$_['column_sort_order']    = 'Sort Order';
+$_['column_sort_order']    = 'Ordre de tri';
 $_['column_action']        = 'Action';
 
 // Entry
-$_['entry_name']           = 'Custom Field Name';
-$_['entry_location']       = 'Location';
+$_['entry_name']           = 'Nom du champ personnalisé';
+$_['entry_location']       = 'Emplacement';
 $_['entry_type']           = 'Type';
-$_['entry_value']          = 'Value';
+$_['entry_value']          = 'Valeur';
 $_['entry_validation']     = 'Validation';
 $_['entry_custom_value']   = 'Custom Field Value Name';
 $_['entry_customer_group'] = 'Customer Group';
 $_['entry_required']       = 'Required';
-$_['entry_status']         = 'Status';
+$_['entry_status']         = 'Statut';
 $_['entry_sort_order']     = 'Sort Order';
 
 // Help
