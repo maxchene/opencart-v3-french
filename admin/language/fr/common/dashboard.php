32a9b530cf14738383cbe3a8d18a62f6f9171b12
Kaliel <maxime@kaliel.fr>
2018-02-08 14:31:39 +0100

    left column

--- admin/language/fr/common/dashboard.php
+++ admin/language/fr/common/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Dashboard';
+$_['heading_title'] = 'Tableau de bord';
 
 // Error
-$_['error_install'] = 'Warning: Install folder still exists and should be deleted for security reasons!';
+$_['error_install'] = 'Attention: Le dossier "Install" est toujours présent. Ce dossier doit être supprimé pour des raison de sécurité !';
